4f12befd11c87b2555c330f53c342664deee95cb
savastosic <90868835+savastosic@users.noreply.github.com>
2021-12-21 17:17:35 -0500

    Added Azure function to repo

---
+++ Azure Function/IoTTrigger/__init__.py
@@ -0,0 +1,51 @@
+# Import Modules
+from typing import List
+import logging
+import gzip
+from io import BytesIO
+import azure.functions as func
+from os import listdir
+import os
+import json
+
+# Define Event Hub Connection Details
+logEventHubString = os.environ["logEventHubString"]
+logEventHubName = os.environ["logEventHubName"]
+alarmEventHubString = os.environ["alarmEventHubString"]
+alarmEventHubName = os.environ["alarmEventHubName"]
+realtimeEventHubString = os.environ["realtimeEventHubString"]
+realtimeEventHubName = os.environ["realtimeEventHubName"]
+
+# Get Message from IoT Hub
+def main(event: List[func.EventHubEvent]) -> str:
+
+    # Log Event Details
+    logging.info(f'  EnqueuedTimeUtc = {event.enqueued_time}')
+    
+    # Unzip and Decode GZIP File
+    buf = BytesIO(event.get_body())
+
+    # Open GPZIP File
+    with gzip.open(buf, 'rb') as f_in:
+        file_content = f_in.read()
+        file_content = file_content.decode("utf-8")
+        result = json.loads(file_content)
+
+        try:
+            # Process Realtime File
+            if 'tagId' in result[0]:
+                num_messages = len(result)
+                logging.info(f'There were {num_messages} rows of data in this file')
+
+                data = json.dumps(result)
+                logging.info("File Processed")
+
+                return data
+
+            else:
+                logging.info("Not Realtime File")
+                pass
+
+        except IndexError:
+            logging.info("Empty File Detected")
+            pass
